d2a5c0ebdb50af863955f2a29af040d62b7eb791
nihal238 <nihalthumar1999@gmail.com>
2026-06-02 17:17:28 +0530

    update in hello

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log(`Successfully connnected to mongoDB 👍`);
+    console.log('hello') 
   } catch (error) {
     console.error(`ERROR: ${error.message}`);
     process.exit(1);
